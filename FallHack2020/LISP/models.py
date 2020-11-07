fd482dbb900c0689b596523ab4ddb0df9696dc24
KeeganMcGinnis2 <keeganmcginnis219@gmail.com>
2020-11-07 12:19:56 -0800

    added basic schema for models

--- FallHack2020/LISP/models.py
+++ FallHack2020/LISP/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
+CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('U', 'Unisex'),
+    ('H', 'Handicap')
+)
+
 class Rating(models.Model):
-    rating = Models.IntegerField()
-    # location
-    # user
+    rating = models.IntegerField()
+    location = models.ForeignKey('Coordinate', on_delete=models.CASCADE)
+    washroom_type = models.CharField(max_length=8, choices=CHOICES, default='U')
+
+class Coordinate(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    
